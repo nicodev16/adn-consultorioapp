778947bc219dc18fd6d20ec526ba86cba609e337
Nicolas Martin <nicolas.martin@ceiba.com.co>
2021-10-08 08:46:28 -0500

    se ajusta importación en loginservice

--- src/app/feature/login/shared/services/login/login.service.ts
+++ src/app/feature/login/shared/services/login/login.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpService } from '@core/services/http.service';
 import { environment } from 'src/environments/environment';
-import { ReqLogin } from '../../models/ReqLogin';
+import { ReqLogin } from '../../models/reqLogin';
 import { Token } from '../../models/token';
 
 
